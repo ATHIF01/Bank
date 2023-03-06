1949f4cb3662ef8ea819c75019aa08029b6ec6a2
Athif <athifmuhammed338.com>
2023-03-06 12:18:37 +0530

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,8 +11,8 @@ export class LoginComponent {
 
   //acno=""
   // or
-  acno: any
-  psw: any
+  // acno: any
+  // psw: any
 
   userDetails : any= {
     1000: { username: "anu", acno: 1000, password: "abc123", balance: 0 },
@@ -22,34 +22,56 @@ export class LoginComponent {
 
   }
 
-  login() {
-  var acnum=this.acno
-  var psw=this.psw
-  var userDetails=this.userDetails
-  if(acnum in userDetails){
-if(psw==userDetails[acnum]["password"]){
-  alert('login success')
-}
-else{
-  alert('incurrect password')
-}
+//   login() {
+//   var acnum=this.acno
+//   var psw=this.psw
+//   var userDetails=this.userDetails
+//   if(acnum in userDetails){
+// if(psw==userDetails[acnum]["password"]){
+//   alert('login success')
+// }
+// else{
+//   alert('incurrect password')
+// }
+//   }
+//   else{
+//     alert('incurrect acc number')
+//   }
+
+//   }
+
+  // acnoChange(event: any) {
+  //   this.acno = event.target.value
+  //   console.log(this.acno);
+
+
+  // }
+  // passChange(event:any){
+  //   this.psw= event.target.value
+  //   console.log(this.psw);
+    
+  // }
+
+
+  login(acnum:any,psw:any) {
+    var acnum=acnum.value
+    var psw=psw.value
+    var userDetails=this.userDetails
+    if(acnum in userDetails){
+  if(psw==userDetails[acnum]["password"]){
+    alert('login success')
   }
   else{
-    alert('incurrect acc number')
+    alert('incurrect password')
   }
+    }
+    else{
+      alert('incurrect acc number')
+    }
+  
+    }
 
-  }
-
-  acnoChange(event: any) {
-    this.acno = event.target.value
-    console.log(this.acno);
 
 
-  }
-  passChange(event:any){
-    this.psw= event.target.value
-    console.log(this.psw);
-    
-  }
 
 }
